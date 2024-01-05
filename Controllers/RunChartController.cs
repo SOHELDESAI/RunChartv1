396dadb8b2f4da4704a7bd696a34c48f318dcb99
SOHELDESAI <soheldesai000@gmail.com>
2024-01-05 15:32:32 +0530

    chart line not visible

--- Controllers/RunChartController.cs
+++ Controllers/RunChartController.cs
@@ -6,8 +6,18 @@ using System.Linq;
 
 public class RunChartController : Controller
 {
+    private readonly IConfiguration _configuration;
+    private readonly Random _random = new Random();
+
+    public RunChartController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public IActionResult Index()
     {
+        var dataRefreshIntervalMinutes = _configuration.GetValue<int>("RunChartSettings:DataRefreshIntervalMinutes");
+
         // Generate sample data (x-values: 1 to 10, y-values: random)
         var random = new Random();
         var dataPoints = Enumerable.Range(1, 10)
@@ -15,5 +25,16 @@ public class RunChartController : Controller
             .ToList();
 
         return View(dataPoints);
+    }
+
+    [HttpGet]
+    public IActionResult RefreshData()
+    {
+        // Generate new data for the refresh
+        var newData = Enumerable.Range(1, 10)
+            .Select(x => new DataPoint { X = x, Y = _random.Next(1, 100) })
+            .ToList();
+
+        return Json(newData);
     }
 }
