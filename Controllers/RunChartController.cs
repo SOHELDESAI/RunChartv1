14d36262a338533544ac78f8f0cfd386bf9782a9
SOHELDESAI <soheldesai000@gmail.com>
2024-01-08 21:17:23 +0530

    before adding procedure

--- Controllers/RunChartController.cs
+++ Controllers/RunChartController.cs
@@ -2,6 +2,7 @@
 using RunChartv1.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 
 public class RunChartController : Controller
@@ -17,14 +18,59 @@ public class RunChartController : Controller
     public IActionResult Index()
     {
         var dataRefreshIntervalMinutes = _configuration.GetValue<int>("RunChartSettings:DataRefreshIntervalMinutes");
+        var connectionstrings = _configuration.GetValue<string>("connectionstrings:defaultconnection");
+
+        string query = "SELECT *,100 as Max,80 as USL ,60 as UCL,50 as Mean,30 as LCL,10 as LSL,0 as Min FROM runchartdata";
+
+
+        try
+        {
+            List< DataPoint > points = new List< DataPoint >();
+            using (SqlConnection connection = new SqlConnection(connectionstrings))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            points.Add(new DataPoint()
+                            {
+
+                                X = Convert.ToDouble(reader["X"]),
+                                Y = Convert.ToDouble(reader["Y"]),
+                                Max = Convert.ToDouble(reader["Max"]),
+                                USL = Convert.ToDouble(reader["USL"]),
+                                UCL = Convert.ToDouble(reader["UCL"]),
+                                Mean = Convert.ToDouble(reader["Mean"]),
+                                LCL = Convert.ToDouble(reader["LCL"]),
+                                LSL = Convert.ToDouble(reader["LSL"]),
+                                Min = Convert.ToDouble(reader["Min"]),
+                            });
+                            // Process each row
+                            //Console.WriteLine($"{reader["ColumnName1"]}, {reader["ColumnName2"]}");
+                            // Add more columns as needed
+                        }
+                        return View(points);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
 
         // Generate sample data (x-values: 1 to 10, y-values: random)
-        var random = new Random();
-        var dataPoints = Enumerable.Range(1, 10)
-            .Select(x => new DataPoint { X = x, Y = random.Next(1, 100) , Max=100,USL = 80,UCL=60,Mean=50,LCL=30,LSL=10,Min=0})
-            .ToList();
+        //var random = new Random();
+        //var dataPoints = Enumerable.Range(1, 10)
+        //    .Select(x => new DataPoint { X = x, Y = random.Next(1, 100) , Max=100,USL = 80,UCL=60,Mean=50,LCL=30,LSL=10,Min=0})
+        //    .ToList();
 
-        return View(dataPoints);
+       return View();
     }
 
     [HttpGet]
